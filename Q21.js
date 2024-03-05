b3eab709fbbbc3ae1252b6aef5e667fc312bc3d1
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-03-05 10:39:31 -0800

    Q-21 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-21

---
+++ Q21.js
@@ -0,0 +1,5 @@
+"use strict";
+// 21.	They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items .
+Object.defineProperty(exports, "__esModule", { value: true });
+let person = { name: "Ali", fname: "zafar", age: 30 };
+console.log(person);
